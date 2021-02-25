136e789c462364897856eb47a9424ba03b82a9cf
Af1a1 <aflalahamed0124@gmail.com>
2021-02-25 11:36:49 +0530

    Search completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './component/layout/Navbar';
 import User from './component/users/User';
+import Search from './component/users/Search';
+import Alert from './component/layout/Alert';
 import './App.css';
 import axios from 'axios';
 
@@ -8,19 +10,53 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null
   };
-  async componentDidMount() {
+
+  // async componentDidMount() {
+
+  //   //console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  searchUsers = async (text) => {
+    //console.log(text);
+
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+    console.log(res.items);
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) =>{
+    this.setState({alert: {msg, type }});
+    setTimeout(() => this.setState({alert:null}), 5000);
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
-        <User loading={this.state.loading} users={this.state.users}/>
+        <Alert alert={this.state.alert} />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={users.length > 0 ? true : false}
+          setAlert={this.setAlert}
+        />
+        <User loading={loading} users={users} />
       </div>
     );
   }
--- src/component/layout/Spinner.js
+++ src/component/layout/Spinner.js
@@ -1,8 +1,13 @@
-import React, {Fragment}from 'react'
-import spinner from './spinner.gif'
-export const Spinner = () => <Fragment>
-            <img src={spinner} alt="Loading..." style={{ width: '200px', margin:'auto', display:'block'}}/>
-        </Fragment>   
-
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+export const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt='Loading...'
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+    />
+  </Fragment>
+);
 
 export default Spinner;
